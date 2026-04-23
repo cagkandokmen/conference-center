ced811b80f66f24519163d6eecc66c0f478f0070
çağkan dökmen <40979426+cagkandokmen@users.noreply.github.com>
2026-04-23 18:34:50 +0300

    fix: update INTERNAL_SIGNAL_URL in ecosystem.config.js for production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,8 +24,8 @@ module.exports = {
       env_production: {
         NODE_ENV: 'production',
         PORT: 3002,
-        // Update IVR to connect to the HTTPS signal server using the public domain to prevent SSL local mismatch
-        SIGNAL_SERVICE_URL: 'https://cagkanvideo.duckdns.org',
+        // Use the public domain so SSL certificates match correctly
+        INTERNAL_SIGNAL_URL: 'https://cagkanvideo.duckdns.org',
         // Update this path to where you placed the model on the Ubuntu server!
         VOSK_MODEL_PATH: './models/vosk-model-small-en-us-0.15',
       },
